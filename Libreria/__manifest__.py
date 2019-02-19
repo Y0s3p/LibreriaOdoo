82abc9e83f11cf310b5c65c9d73d5ed77aae958b
Jose Fernandez Garrido <34067926+Y0s3p@users.noreply.github.com>
2019-02-19 17:13:36 +0100

    Add files via upload

---
+++ Libreria/__manifest__.py
@@ -0,0 +1,8 @@
+{
+    'name': 'libreria',
+    'summary': 'módulo libreria',
+    'author': 'Jose Fernandez Garrido',
+    'version': '1.0',
+    'depends': ['base'],
+    'data': ['views/librero.xml', 'views/libro.xml', 'views/cliente.xml', 'views/categoria.xml']
+}
